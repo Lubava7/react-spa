a661f160ee93ac632c54519f73e4134d970f7245
lubava <joheunsaram@mail.ru>
2022-01-28 17:57:08 +0300

    add a class component button

--- src/components/Buttons/Buttons.js
+++ src/components/Buttons/Buttons.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import './Buttons.css'
 
+// Ниже у меня функциональный компонент:
+// const Buttons = (props) => {
+//     return(
+//         <div>
+//         <h1>{props.title}</h1>
+//         <button>{props.text}</button>
+//         </div>
+//     )
+//   };
 
-const Buttons = (props) => {
-    return(
-        <div>
-        <h1>{props.title}</h1>
-        <button>{props.text}</button>
-        </div>
-    )
-  };
+
+
+// Ниже будет классовый компонент(СДелать также стрелочную функцию)
+
+
+class Buttons extends React.Component{
+    render(){
+        return(
+            <div>
+            <h1>{this.props.title}</h1>
+            <button>{this.props.text}</button>
+            </div>
+        )
+    }
+}
  
 
   export default Buttons;
